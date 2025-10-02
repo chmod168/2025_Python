d1a55cd98634f0a2a2a0929f787f442c961b4c09
chmod166 <sthdifferent847@gmail.com>
2025-10-02 11:13:42 +0900

    커밋

--- 과제2/place.py
+++ 과제2/place.py
@@ -4,15 +4,12 @@ root=Tk()
 root.geometry("300x100")
 
 button1=Button(root,text="버튼1",bg="red",fg="white")
-button1.grid(row=0,column=0)
+button1.place(x=0,y=0)
 
 button2=Button(root,text="버튼2",bg="green",fg="black")
-button2.grid(row=0,column=1)
+button2.place(x=30,y=30)
 
 button3=Button(root,text="버튼3",bg="blue",fg="white")
-button3.grid(row=1,column=0)
-
-button4=Button(root,text="버튼4",bg="yellow",fg="red")
-button4.grid(row=1,column=1)
+button3.place(x=60,y=60)
 
 root.mainloop()
